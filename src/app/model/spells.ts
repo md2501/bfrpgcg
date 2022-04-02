35f952ace059eb2da82a48812a6f84c88b1177f3
md2501 <1756563+md2501@users.noreply.github.com>
2022-04-02 20:02:39 +0200

    add all mu spells

--- src/app/model/spells.ts
+++ src/app/model/spells.ts
@@ -1,13 +1,113 @@
 export const magicUserSpells: string[][] = [[
+    // 1. Level
     "Charm Person",
     "Detect Magic",
     "Floating Disc",
     "Hold Portal",
-    "Light",
+    "Light*",
     "Magic Missile",
     "Magic Mouth",
-    "Protection from Evil",
+    "Protection from Evil*",
     "Read Languages",
     "Shield",
     "Sleep",
-    "Ventriloquism"]];
+    "Ventriloquism"],
+// 2. Level
+[
+    "Continual Light*",
+    "Detect Evil*",
+    "Detect Invisible",
+    "ESP",
+    "Invisibility",
+    "Knock",
+    "Levitate",
+    "Locate Object",
+    "Mirror Image",
+    "Phantasmal Force",
+    "Web",
+    "Wizard Lock",
+],
+// 3. Level
+[
+    "Clairvoyance",
+    "Darkvision",
+    "Dispel Magic",
+    "Fireball",
+    "Fly",
+    "Haste*",
+    "Hold Person",
+    "Invisibility 10‘ radius",
+    "Lightning Bolt",
+    "Protection from Evil 10’ radius*",
+    "Protection from Normal Missiles",
+    "Water Breathing",
+],
+// 4. Level
+[
+    "Charm Monster",
+    "Confusion",
+    "Dimension Door",
+    "Growth of Plants*",
+    "Hallucinatory Terrain",
+    "Ice Storm",
+    "Massmorph",
+    "Polymorph Other",
+    "Polymorph Self",
+    "Remove Curse*",
+    "Wall of Fire",
+    "Wizard Eye",
+],
+// 5. Level
+[
+    "Animate Dead",
+    "Cloudkill",
+    "Conjure Elemental",
+    "Feeblemind",
+    "Hold Monster",
+    "Magic Jar",
+    "Passwall",
+    "Telekinesis",
+    "Teleport",
+    "Wall of Stone",
+],
+// 6. Level
+[
+    "Anti-Magic Shell",
+    "Death Spell",
+    "Disintegrate",
+    "Flesh to Stone*",
+    "Geas*",
+    "Invisible Stalker",
+    "Lower Water",
+    "Projected Image",
+    "Reincarnate",
+    "Wall of Iron",
+]
+];
+
+export const clericSpells: string[][] = [
+    // 1. Level
+    [
+
+    ],
+    // 2. Level
+    [
+
+    ],
+    // 3. Level
+    [
+
+    ],
+    // 4. Level
+    [
+
+    ],
+    // 5. Level
+    [
+
+    ],
+    // 6. Level
+    [
+
+    ],
+]
