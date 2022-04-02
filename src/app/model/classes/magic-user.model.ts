3c32af81ea87fd1c6dda607eebba9831481651a3
md2501 <1756563+md2501@users.noreply.github.com>
2022-04-02 21:54:37 +0200

    add saves for mu

--- src/app/model/classes/magic-user.model.ts
+++ src/app/model/classes/magic-user.model.ts
@@ -6,8 +6,14 @@ import IClass from "./iclass.interface";
 export class MagicUser implements IClass {
     name = ClassName.MagicUser;
     abilityRequirements = { [AbilityName.INTELLIGENCE]: 9 };
-    hpBonus = {10: 1, 11: 2, 12: 3, 13: 4, 14: 5, 15: 6, 16: 7, 17: 8, 18: 9, 19: 10, 20: 11};
-    savingThrow = { [SavingThrowName.POISON]: 13, [SavingThrowName.WANDS]: 14, [SavingThrowName.PARALYSIS]: 13, [SavingThrowName.DRAGONBREATH]: 16, [SavingThrowName.SPELLS]: 15 };
+    hpBonus = { 10: 1, 11: 2, 12: 3, 13: 4, 14: 5, 15: 6, 16: 7, 17: 8, 18: 9, 19: 10, 20: 11 };
+    savingThrow = {
+        [SavingThrowName.POISON]: [13, 13, 12, 12, 11, 11, 10, 10, 9, 9, 8],
+        [SavingThrowName.WANDS]: [14, 14, 13, 12, 11, 10, 10, 9, 8, 7, 6],
+        [SavingThrowName.PARALYSIS]: [13, 13, 12, 11, 10, 9, 9, 8, 7, 6, 5],
+        [SavingThrowName.DRAGONBREATH]: [16, 15, 15, 14, 14, 13, 13, 12, 12, 11, 11],
+        [SavingThrowName.SPELLS]: [15, 14, 13, 13, 12, 11, 11, 10, 9, 9, 8]
+    };
     hd = 4;
     ab = 1;
     weaponRestrictions = ["may not wear armor nor use a shield", "may use no weapon besides dagger, staff or cudgel"];
@@ -15,23 +21,23 @@ export class MagicUser implements IClass {
     spells = {
         1: [1],
         2: [2],
-        3: [2,1],
-        4: [2,2],
-        5: [2,2,1],
-        6: [3,2,2],
-        7: [3,2,2,1],
-        8: [3,3,2,2],
-        9: [3,3,2,2,1],
-        10: [4,3,3,2,2],
-        11: [4,4,3,2,2,1],
-        12: [4,4,3,3,2,2],
-        13: [4,4,4,3,2,2],
-        14: [4,4,4,3,3,2],
-        15: [5,4,4,3,3,2],
-        16: [5,5,4,3,3,2],
-        17: [5,5,4,4,3,3],
-        18: [6,5,4,4,3,3],
-        19: [6,5,5,4,3,3],
-        20: [6,5,5,4,4,3]
-     };
+        3: [2, 1],
+        4: [2, 2],
+        5: [2, 2, 1],
+        6: [3, 2, 2],
+        7: [3, 2, 2, 1],
+        8: [3, 3, 2, 2],
+        9: [3, 3, 2, 2, 1],
+        10: [4, 3, 3, 2, 2],
+        11: [4, 4, 3, 2, 2, 1],
+        12: [4, 4, 3, 3, 2, 2],
+        13: [4, 4, 4, 3, 2, 2],
+        14: [4, 4, 4, 3, 3, 2],
+        15: [5, 4, 4, 3, 3, 2],
+        16: [5, 5, 4, 3, 3, 2],
+        17: [5, 5, 4, 4, 3, 3],
+        18: [6, 5, 4, 4, 3, 3],
+        19: [6, 5, 5, 4, 3, 3],
+        20: [6, 5, 5, 4, 4, 3]
+    };
 }
