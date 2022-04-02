702c135261adcdfeef238834ea798f31ea8a92b7
md2501 <1756563+md2501@users.noreply.github.com>
2022-04-02 21:46:24 +0200

    add saves for fighter

--- src/app/model/classes/fighter.model.ts
+++ src/app/model/classes/fighter.model.ts
@@ -6,7 +6,13 @@ import IClass from "./iclass.interface";
 export class Fighter implements IClass {
     name= ClassName.Fighter;
     hpBonus = {10: 2, 11: 4, 12: 6, 13: 8, 14: 10, 15: 12, 16: 14, 17: 16, 18: 18, 19: 20, 20: 22};
-    savingThrow= { [SavingThrowName.POISON]: 12, [SavingThrowName.WANDS]: 13, [SavingThrowName.PARALYSIS]: 14, [SavingThrowName.DRAGONBREATH]: 15, [SavingThrowName.SPELLS]: 17, };
+    savingThrow= { 
+        [SavingThrowName.POISON]: [12, 11, 11, 10, 9, 9, 8, 7, 7, 6, 5],
+        [SavingThrowName.WANDS]: [13, 12, 11, 11, 10, 9, 9, 8, 7, 7, 6],
+        [SavingThrowName.PARALYSIS]: [14, 14, 13, 12, 12, 11, 10, 10, 9, 8, 8],
+        [SavingThrowName.DRAGONBREATH]: [15, 15, 14, 14, 13, 12, 12, 11, 10, 10, 9],
+        [SavingThrowName.SPELLS]: [17, 16, 15, 15, 14, 13, 13, 12, 11, 11, 10]
+    };
     hd = 8;
     ab = 1;
     abilityRequirements = { [AbilityName.STRENGTH]: 9 };
