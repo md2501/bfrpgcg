69321b7a34e6563588504708f86db98913e86b38
f-kraft <fabian.kraft@protonmail.com>
2022-04-04 19:59:07 +0200

    * added english translations for headings and form fields * forgot to switch to i18n branch :(

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -10,10 +11,11 @@ import { TranslocoRootModule } from './transloco-root.module';
     AppComponent,
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    TranslocoRootModule
+    TranslocoRootModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
