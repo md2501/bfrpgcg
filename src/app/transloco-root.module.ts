88fd661f02aa46c3ef5db4c5445ce70ddc5ad38c
f-kraft <fabian.kraft@protonmail.com>
2022-04-05 21:28:24 +0200

    Set fallback language to english

--- src/app/transloco-root.module.ts
+++ src/app/transloco-root.module.ts
@@ -29,6 +29,7 @@ export class TranslocoHttpLoader implements TranslocoLoader {
       useValue: translocoConfig({
         availableLangs: ['en', 'de'],
         defaultLang: 'en',
+        fallbackLang: 'en',
         // Remove this option if your application
         // doesn't support changing language in runtime.
         reRenderOnLangChange: true,
